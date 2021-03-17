fe8db309b2abe71ceaef58bc78f97f2e7b9f0c63
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2021-03-17 13:20:42 +0000

    Created fragments and navigation bar

--- app/src/main/java/com/example/cs4084_mobile_application_development/MainActivity.java
+++ app/src/main/java/com/example/cs4084_mobile_application_development/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cs4084_mobile_application_development;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottom_nav = findViewById(R.id.bottom_navigation);
+        bottom_nav.setOnNavigationItemSelectedListener(navListener);
+
+        //Default fragment when app is first opened
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_leaderboard:
+                            selectedFragment = new LeaderboardFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_directions_run:
+                            selectedFragment = new StatisticFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment ).commit();
+
+                    return true;
+                }
+            };
 }
