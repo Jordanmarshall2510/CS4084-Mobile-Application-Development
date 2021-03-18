5d359c43246a8b8b05b3b2cb6b1e4377319dcc96
cleamy432 <18256112@studentmail.ul.ie>
2021-03-18 15:43:39 +0000

    Added home page layout and renamed icons
    Added home page layout and renamed icons

--- app/src/main/java/com/example/cs4084_mobile_application_development/HomeFragment.java
+++ app/src/main/java/com/example/cs4084_mobile_application_development/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.cs4084_mobile_application_development;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,4 +18,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstaceState) {
+
+        super.onViewCreated(view, savedInstaceState);
+
+        View currentView = getView();
+
+        // Get references to text views we want to change
+        TextView distanceText = (TextView) currentView.findViewById(R.id.distanceText);
+        TextView caloriesText = (TextView) currentView.findViewById(R.id.caloriesText);
+        TextView timeText = (TextView) currentView.findViewById(R.id.timeText);
+
+        // Get references to image views we want to change
+        ImageView distanceImage = (ImageView) currentView.findViewById(R.id.distanceImage);
+        ImageView caloriesImage = (ImageView) currentView.findViewById(R.id.caloriesImage);
+        ImageView timeImage = (ImageView) currentView.findViewById(R.id.timeImage);
+
+        // Change the text
+        distanceText.setText("1.05");
+        caloriesText.setText("150");
+        timeText.setText("0h 25m");
+
+        // Change the image colours
+        distanceImage.setColorFilter(Color.parseColor("#ff3030"));
+        caloriesImage.setColorFilter(Color.parseColor("#fba00e"));
+        timeImage.setColorFilter(Color.parseColor("#0099ff"));
+
+    }
+
 }
