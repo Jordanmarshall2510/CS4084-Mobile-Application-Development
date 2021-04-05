019e823f536077712fa28d5d810aadd48ce14594
Shane Donaghy <shanedon2017@gmail.com>
2021-04-05 12:09:58 +0100

    Adding signup page

--- app/src/main/java/com/example/cs4084_mobile_application_development/SignUpActivity.java
+++ app/src/main/java/com/example/cs4084_mobile_application_development/SignUpActivity.java
@@ -7,34 +7,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
-        Button btn1 = (Button)findViewById(R.id.loginSubmit);
+        Button btn1 = (Button)findViewById(R.id.signupSubmit);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 finish();
-
             }
         });
 
-        Button btn2 = (Button)findViewById(R.id.signupSubmit);
+
+
+        Button btn2 = (Button)findViewById(R.id.goBack);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent k = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent k = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(k);
-
             }
         });
+
+
     }
 }
