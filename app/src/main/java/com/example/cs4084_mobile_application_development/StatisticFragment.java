794432d3cb763558e3dcc77be53969f2fdbc2a73
RLowry2 <47782376+RLowry2@users.noreply.github.com>
2021-04-07 20:56:21 +0100

    Took stat data from database

--- app/src/main/java/com/example/cs4084_mobile_application_development/StatisticFragment.java
+++ app/src/main/java/com/example/cs4084_mobile_application_development/StatisticFragment.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
 
+import com.example.Database.Database;
 import com.google.android.gms.maps.model.LatLng;
 
 public class StatisticFragment extends Fragment {
@@ -30,6 +30,9 @@ public class StatisticFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        Database database = Database.getInstance();
+
         super.onViewCreated(view, savedInstanceState);
         View currentView = getView();
         locationPoints = LocationService.getLocationPoints();
@@ -45,24 +48,48 @@ public class StatisticFragment extends Fragment {
         TextView totalSpeedTextStatistic = (TextView) currentView.findViewById(R.id.totalSpeedTextStatistic);
         TextView totalCaloriesBurntTextStatistic = (TextView) currentView.findViewById(R.id.totalCaloriesBurntTextStatistic);
 
+        //Gets the time in minutes and hours for daily time
+        double dailyTimeMinutes = (((double) database.getDailyTime() / 1000) / 60);
+        double dailyTimeHours = ((((double) database.getDailyTime() / 1000) / 60) - dailyTimeMinutes) / 60;
+
+        dailyTimeMinutes = Math.round(dailyTimeMinutes);
+
+        //Gets the speed in meters per second for daily speed
+        double dailyTime = (double) database.getDailyTime();
+        double dailyDistance = (double) database.getDailyDistance();
+        double dailySpeed = dailyDistance / dailyTime;
+        double dailySpeedTotal = dailySpeed * 1000;
+
         // Change the daily text
         dailyDistanceTextStatistic
-                .setText("Distance:    " + distance + "km");
+                .setText("Distance:    " + ((double) database.getDailyDistance() / 1000) + "km");
         dailyTimeTextStatistic
-                .setText("Time:           " + time + "");
+                .setText("Time:           " + dailyTimeHours +"h " + dailyTimeMinutes + "m");
         dailySpeedTextStatistic
-                .setText("Speed:         " + speed + "");
+                .setText("Speed:         " + ((double) Math.round(dailySpeedTotal * 100) / 100) + "m/s");
         dailyCaloriesBurntTextStatistic
-                .setText("Calories:      " + calories + "kcal");
+                .setText("Calories:      " + database.getDailyCalories() + "kcal");
+
+        //Gets the Time in minutes and hours for total time
+        double totalTimeMinutes = (((double) database.getTotalTime() / 1000) / 60);
+        double totalTimeHours = ((((double) database.getTotalTime() / 1000) / 60) - totalTimeMinutes) / 60;
+
+        totalTimeMinutes = Math.round(totalTimeMinutes);
+
+        //Gets the speed in meters per second for the total speed
+        double totalTime = (double) database.getTotalTime();
+        double totalDistance = (double) database.getTotalDistance();
+        double totalSpeed = totalDistance / totalTime;
+        double speedTotal = totalSpeed * 1000;
 
         //Change the total text
         totalDistanceTextStatistic
-                .setText("Distance:    " + distance + "km");
+                .setText("Distance:    " + (((double) database.getTotalDistance())/ 1000) + "km");
         totalTimeTextStatistic
-                .setText("Time:           " + time + "");
+                .setText("Time:           " + totalTimeHours + "h " + totalTimeMinutes + "m");
         totalSpeedTextStatistic
-                .setText("Speed:         " + speed + "");
+                .setText("Speed:         " + ((double) Math.round(speedTotal * 100) / 100) + "m/s");
         totalCaloriesBurntTextStatistic
-                .setText("Calories:      " + calories + "kcal");
+                .setText("Calories:      " + database.getTotalCalories() + "kcal");
     }
 }
