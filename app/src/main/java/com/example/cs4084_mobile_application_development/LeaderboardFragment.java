19d58049cab55c1f0a5cca631ce0e8f02fc8428d
Jordan Marshall <47557144+Jordanmarshall2510@users.noreply.github.com>
2021-04-08 16:51:46 +0100

    Added values to xml

--- app/src/main/java/com/example/cs4084_mobile_application_development/LeaderboardFragment.java
+++ app/src/main/java/com/example/cs4084_mobile_application_development/LeaderboardFragment.java
@@ -1,9 +1,11 @@
 package com.example.cs4084_mobile_application_development;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,13 +25,51 @@ public class LeaderboardFragment extends Fragment {
         Database database = Database.getInstance();
 
         super.onViewCreated(view, savedInstanceState);
+        View currentView = getView();
 
-        new Thread(new Runnable() {
+        TextView row1 = (TextView) currentView.findViewById(R.id.row1);
+        TextView row2 = (TextView) currentView.findViewById(R.id.row2);
+        TextView row3 = (TextView) currentView.findViewById(R.id.row3);
+        TextView row4 = (TextView) currentView.findViewById(R.id.row4);
+        TextView row5 = (TextView) currentView.findViewById(R.id.row5);
+        TextView row6 = (TextView) currentView.findViewById(R.id.row6);
+        TextView row7 = (TextView) currentView.findViewById(R.id.row7);
+        TextView row8 = (TextView) currentView.findViewById(R.id.row8);
+        TextView row9 = (TextView) currentView.findViewById(R.id.row9);
+        TextView row10 = (TextView) currentView.findViewById(R.id.row10);
+
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                database.getLeaderboard();
+                Leaderboard.leaderboardValues = database.getLeaderboard();
             }
-        }).start();
+        });
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String [] values = Leaderboard.leaderboardValues;
+        TextView[] textViews =
+        {
+            row1, row2, row3, row4, row5, row6, row7, row8, row9, row10
+        };
+
+        for(int i=0; i<10; i++){
+            textViews[i].setText(values[i]);
+            if (values[i].contains("ME")){
+                textViews[i].setBackgroundColor(Color.parseColor("#00C6BE"));
+                textViews[i].setTextColor(Color.parseColor("black"));
+            }
+        }
 
     }
+}
+
+class Leaderboard {
+    public static String[] leaderboardValues;
 }
